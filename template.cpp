2e5125eed538eebe30a744b172d838acb383de38
Dongxu Wang <wangdongxuking61@gmail.com>
2017-07-25 11:36:31 +0800

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <array>
+#include <vector>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cstdint>
+#include <cmath>
+
+using namespace std;
+
+#define to_str(var)                     #var" = %2d   "
+#define PRINT_1VAR(v0)                  printf(to_str(v0)"\n",v0)
+#define PRINT_2VAR(v0, v1)              printf(to_str(v0)"\t" to_str(v1)"\n",v0,v1)
+#define PRINT_3VAR(v0, v1, v2)          printf(to_str(v0)"\t" to_str(v1)"\t" to_str(v2)"\n",v0,v1,v2)
+#define PRINT_4VAR(v0, v1, v2, v3)      printf(to_str(v0)"\t" to_str(v1)"\t" to_str(v2)"\t" to_str(v3)"\n",v0,v1,v2,v3)
+
+
+int main()
+{
+	return 0;
+}
